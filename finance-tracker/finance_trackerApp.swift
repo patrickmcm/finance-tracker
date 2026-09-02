c42a9902af4c685f638276691d10138b9ee5de98
patrickmcm <patrickmcmanamon101@gmail.com>
2026-09-02 10:17:40 +0100

    Initial Commit

---
+++ finance-tracker/finance_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  finance_trackerApp.swift
+//  finance-tracker
+//
+//  Created by Patrick McManamon on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct finance_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
